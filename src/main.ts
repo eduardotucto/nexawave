99e590128aaedc568c073c2c36e72d8aeb1ab0c1
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-08-30 18:35:13 -0500

    install @nestjs/typeorm typeorm pg

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,22 @@ async function bootstrap () {
   await app.listen(3000)
 }
 bootstrap()
+
+/**
+
+const postgres = require('postgres');
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const sql = postgres(URL, { ssl: 'require' });
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
+
+ */
