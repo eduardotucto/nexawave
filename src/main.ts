8bc12929d0ed073f17a735ebccc894db8dd68973
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-08-30 18:41:39 -0500

    generate user CRUD with nest g resource

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,3 @@ async function bootstrap () {
   await app.listen(3000)
 }
 bootstrap()
-
-/**
-
-const postgres = require('postgres');
-require('dotenv').config();
-
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
-const sql = postgres(URL, { ssl: 'require' });
-
-async function getPgVersion() {
-  const result = await sql`select version()`;
-  console.log(result);
-}
-
-getPgVersion();
-
- */
