c6c36b50e13cd882705b75ffc23661692f615f2f
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-08-30 20:39:21 -0500

    database credentials env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import * as path from 'path'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PGHOST'),
+        port: 5432,
+        username: configService.get('PGUSER'),
+        password: configService.get('PGPASSWORD'),
+        database: configService.get('PGDATABASE'),
+        entities: [path.join(__dirname, '**', '*.entity{.ts,.js}')], // para que cargue automáticamente todos las entidades
+        synchronize: true,
+        logging: true,
+        // esto sería el sslmode=require. Qué se yo (?) xd pero si no lo pongo me da error
+        ssl: true
+      }),
+      inject: [ConfigService]
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
