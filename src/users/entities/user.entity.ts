ffb16699a447ddcc26966c8ddb81e0a1e3a7751b
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-08-30 21:18:15 -0500

    Add User entity Created the User entity with its corresponding columns and decorators.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+    id: string
+
+  @Column({ unique: true })
+    email: string
+
+  @Column()
+    password: string
+
+  @Column({ nullable: true })
+    phone?: string
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+    createdAt: Date
+
+  @UpdateDateColumn({ type: 'timestamp with time zone' })
+    updatedAt: Date
+
+  @Column({ nullable: true })
+    authStrategy?: string
+}
