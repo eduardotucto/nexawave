eb77e60dd804fe6723abc94108b9768565a7a4bd
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-09-02 21:17:00 -0500

    return a token in registration response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,35 @@
 import * as bcrypt from 'bcrypt'
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
 import { type LoginFieldsDto } from './dto/login-fields.dto'
 import { type RegisterFieldsDto } from './dto/register-fields.dto'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor (private readonly usersService: UsersService) {}
+  constructor (
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
   async signIn (loginFieldsDto: LoginFieldsDto) {}
 
   async signUp (registerFieldsDto: RegisterFieldsDto) {
-    const saltOrRounds = 10
-    const password = registerFieldsDto.password
-    const hash = await bcrypt.hash(password, saltOrRounds)
-    const userData = {
-      ...registerFieldsDto,
-      password: hash
+    try {
+      const saltOrRounds = 10
+      const password = registerFieldsDto.password
+      const hash = await bcrypt.hash(password, saltOrRounds)
+      const userData = {
+        ...registerFieldsDto,
+        password: hash
+      }
+      const newUserCreated = await this.usersService.create(userData)
+      const accessToken = await this.jwtService.signAsync({ id: newUserCreated.id })
+      return {
+        accessToken
+      }
+    } catch (e) {
+      throw new BadRequestException(e.message)
     }
-    return this.usersService.create(userData)
   }
 }
