f7022a171f9e81f48a46eaa72f0eef6810abd552
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-09-10 20:31:52 -0500

    trying test

--- src/users/application/users.service.spec.ts
+++ src/users/application/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { UsersService } from './users.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../domain/user.entity'
+import { DatabaseModule } from 'src/database/database.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 describe('UsersService', () => {
   let service: UsersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService]
+      imports: [
+        DatabaseModule,
+        TypeOrmModule.forFeature([User]),
+        ConfigModule
+      ],
+      providers: [UsersService, ConfigService]
     }).compile()
 
     service = module.get<UsersService>(UsersService)
