275e0f42d3c8278f73377c6224a48fb5a654585a
Eduardo Tucto <tuctoeduardo@gmail.com>
2023-09-24 21:30:59 -0500

    valid nanoid in token

--- src/auth/insfrastructure/jwt.strategy.ts
+++ src/auth/insfrastructure/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
 import { jwtConfig } from 'src/config/jwt.config'
 import { ConfigService } from '@nestjs/config'
+import { NanoIdValidationPipe } from 'src/nanoid-validation.pipe'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -15,6 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate (payload: any) {
+    await new NanoIdValidationPipe().transform(payload.id, { type: 'param', metatype: String })
     return { id: payload.id }
   }
 }
